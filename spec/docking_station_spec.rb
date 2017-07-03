33d25b0bb5152ec7b5a71bb2206ba2b4ee84b422
BDCraven <bencraven@hotmail.com>
2017-07-03 21:31:34 +0100

    Red first failing test instances should respond to release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,7 @@
 require 'docking_station'
 describe DockingStation do
-
+  it {is_expected.to respond_to :release_bike }
+    # is_expected is defined simply as expect(subject)
+    # the subject is instances of the DockingStation class
+    # the test is testing if instances respond to the method release_bike
 end
