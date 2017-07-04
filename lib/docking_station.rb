9bf501b796346437d0629c33b9950f0e17c7ef82
Cong Cong Bo <cong@hippocampus.ai>
2017-07-04 15:49:47 +0100

    Failing tests because no bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,18 +1,23 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
+  #attr_reader :bike
+
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-    fail 'No bikes available' unless @bike 
-    @bike
+    fail 'No bikes available' if @bikes.empty?
+    @bike.pop
   end
 
   def dock(bike)
+    fail 'Docking station full' if @bike.count >= 20
     # we need to return the bike we dock
     # Use an instance variable to store the bike
     # in the 'state' of this instance
-    @bike = bike
+    @bike << bike
   end
 
   # add to bike method to return the bike we docked
--- spec/feature_spec.rb
+++
@@ -1,3 +0,0 @@
-require './lib/docking_station'
-docking_station = DockingStation.new
-docking_station.release_bike
