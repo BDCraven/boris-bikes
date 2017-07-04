16103832ad4e26e348cd0e372f117e623aedce05
BDCraven <bencraven@hotmail.com>
2017-07-04 12:39:59 +0100

    Amended bike and docking station classes to make stations get bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,10 @@ describe DockingStation do
     # is_expected is defined simply as expect(subject)
     # the subject is instances of the DockingStation class
     # the test is testing if instances respond to the method release_bike
+
+    it 'releases working bikes' do
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
+
 end
