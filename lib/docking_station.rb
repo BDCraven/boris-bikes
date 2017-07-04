2871bca7d1e2daa926d6855dc09d1d48945329fc
BDCraven <bencraven@hotmail.com>
2017-07-04 14:26:19 +0100

    Add Bike instances using an instance variable and attribute reader to access the variable from the DockingStation

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,21 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
+   
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+    # we need to return the bike we dock
+    # Use an instance variable to store the bike
+    # in the 'state' of this instance
+    @bike = bike
+  end
+
+  # add to bike method to return the bike we docked
+  # def bike
+  #  @bike
+  # end
 end
