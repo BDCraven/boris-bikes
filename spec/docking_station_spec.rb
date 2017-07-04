ecb5cc72ceb0450a951d3ce3b0c1f786c7e3fef3
BDCraven <bencraven@hotmail.com>
2017-07-04 15:10:11 +0100

    Add feature_spec file to specand add test to check error is raised when station is empty

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -25,6 +25,23 @@ describe DockingStation do
       subject.dock(bike)
       #Again we need to return the bike we just docked
       expect(subject.bike).to eq bike
-    end 
+    end
+      # let's upgrade our syntax, to:
+      # use a '#' before a method name
+      # to imply that it is an instance
+      # method. Also look: nested describes!
+    describe '#release_bike' do
+      it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      # we want to release the bike we docked
+      expect(subject.release_bike).to eq bike
+      end
 
+      it 'raises an error when there are no bikes available' do
+        # Let's not dock a bike first:
+        # remember subject = DockingStation.new
+        expect { subject.release_bike }.to raise_error 'No bikes available'
+      end
+    end
 end
